ac057d1f866f70e6a91c611a0b24d217d604ba0d
kobari1996 <momochi.f91.555@gmail.com>
2017-01-10 14:46:35 +0900

    初版
    コンソールアプリ

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("kobari\n");
+	return 0;
+}
+
